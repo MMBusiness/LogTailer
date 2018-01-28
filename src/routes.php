d1b937d3497ddf9fce5d7534a65da8f665bd4807
Mikro Mika <dev.mmcore@gmail.com>
2018-01-28 12:06:56 +0200

    packiagst works again !

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,6 @@
 Route::get('logger', function(){
 	echo 'Hello from the logger package!, Looks like view has registered correctly at src/LogTailerServiceProvider.php';
 });
-Route::get('/pub', function () {
+Route::get('/', function () {
     return view('welcome');
 });
