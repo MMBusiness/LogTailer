cf6973a00c09a4983fa0a59f427beecfe0d098fb
Mikro Mika <dev.mmcore@gmail.com>
2018-01-28 09:48:04 +0200

    packist do not updatecomposer package atomaticly after git update, Why ?

---
+++ src/Config/version.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'name' => 'LogTailer',
+    'comment'=>'Tail for log files via LARAVEL cli',
+    'version_Main' =>'0',
+    'version_Minor' =>'0',
+    'version_compile' =>'1'
+];
