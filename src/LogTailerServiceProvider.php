5b8330eb9ee9efc4e2ec3e3e1241ae3031651fc1
Mikro Mika <dev.mmcore@gmail.com>
2018-01-28 13:36:59 +0200

    $this->loadViewsFrom(__DIR__ . './views', 'LogTailer');   // does not register

--- src/LogTailerServiceProvider.php
+++ src/LogTailerServiceProvider.php
@@ -13,11 +13,10 @@ class LogTailerServiceProvider extends ServiceProvider
         // delicated route file for packages
      include __DIR__.'/routes.php';   // ok
      // - first the published/overwritten views (in case they have any changes)
-     $this->loadViewsFrom(resource_path('views/vendor/LogTailer'), '');
-      $this->loadViewsFrom(realpath(__DIR__.'./Views'), 'LogTailer');
-    //
-    //  $this->loadViewsFrom(__DIR__ . './views', 'logtailer');   // does not register
-      $this->publishes([
+   $this->loadViewsFrom(resource_path('views/vendor/LogTailer'), '');
+    $this->loadViewsFrom(realpath(__DIR__.'./Views'), 'LogTailer');
+
+    $this->publishes([
         __DIR__ . './Views' => base_path('resources/views/vendor/LogTailer')
       ], 'views');
 
