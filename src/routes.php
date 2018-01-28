713459c6f91334dc06ba1e46d0e0b37f314e28a2
Mikro Mika <dev.mmcore@gmail.com>
2018-01-28 09:41:28 +0200

    packist do not updatecomposer package atomaticly after git update, Why ?

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::get('logger', function(){
-	echo 'Hello from the logger package!';
+	echo 'Hello from the logger package!, Looks like view has registered correctly at src/LogTailerServiceProvider.php';
 });
