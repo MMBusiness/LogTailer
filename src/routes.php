2d6a3cb61cb0f2e6257fd1f4b712f0ab68673b10
Mikro Mika <dev.mmcore@gmail.com>
2018-01-29 09:33:34 +0200

    //return view('jihuu');  via package does not work yet. ....

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,5 @@ Route::get('logger', function(){
 	echo 'Hello from the logger package!, Looks like view has registered correctly at src/LogTailerServiceProvider.php';
 });
 Route::get('/foobar', function () {
-    return view('vendor/LogTailer/welcome');
+    return view('LogTailer::layout.home');
 });
