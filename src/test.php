b0ed93cee6c01daa81570525cc91d8088b2cdabb
Mikro Mika <dev.mmcore@gmail.com>
2018-01-29 07:42:09 +0200

    return view('vendor/LogTailer/welcome'); and Route::get('/foobar' from routefile !

--- src/test.php
+++ src/test.php
@@ -9,7 +9,7 @@ trait Test {
     public static function Init()
     {
         Route::get('/test', function(){
-            return view('logtailer/welcome');
+            return view('logtailer/jihuu');
         //  die('Route works from package! namespace Mikromike\LogTailer');
         });
     }
